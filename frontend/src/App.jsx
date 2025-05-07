4eb452700a7a5a28ca6200983934a2d023f9d14f
Christos Zervas <90809713+czervas@users.noreply.github.com>
2025-05-06 23:32:59 -0400

    Added new UI
    autoscrolling down of input from server, animations, added modified TLCengine logo, smoothed out page loading.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import './App.css';
+
+
+
+
 
 const examplePrompts = [
   'Vacation homes with beachfront property',
@@ -15,10 +19,19 @@ function App() {
   const [searching, setSearching] = useState(false);
   const [displayedExamples, setDisplayedExamples] = useState([]);
   const [logText, setLogText] = useState('');
+  const [searchSubmitted, setSearchSubmitted] = useState(false);
+  const textareaRef = useRef(null);
+
   useEffect(() => {
     const shuffled = examplePrompts.sort(() => 0.5 - Math.random());
     setDisplayedExamples(shuffled.slice(0, 3));
+    document.body.style.visibility = 'visible';
   }, []);
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.scrollTop = textareaRef.current.scrollHeight;
+    }
+  }, [logText]); //runs every time logText updates
 
   const search = async (text, e) => {
     if (e) e.preventDefault();
@@ -26,20 +39,36 @@ function App() {
     const url = `http://localhost:5000/search?prompt=${encodeURIComponent(
       text
     )}`;
+    //setFadingOut(true);
+    // let test;
+    // for(i=0;i<50;i++){
+    //   test += 'hello\n';
+    // }
+
     setLogText(
-      `User input:\n${text}\n\nWaiting for response from server...\n\n`
+      `User input:\n${text}\n\nWaiting for response from server...\n`
     );
     const response = await fetch(url);
     const json = await response.json();
     setLogText((x) => `${x}Response:\n${JSON.stringify(json)}`);
+    
     console.log(json);
     setSearching(false);
   };
 
   return (
-    <>
+    <><div id="all"
+        className={`fade-in`}>
+      <div className={`fade-on-submit ${searchSubmitted ? 'fade-out' : ''}`}>
+      {/* {!searchSubmitted && <h1>What are you looking for?</h1>} */}
       <h1>What are you looking for?</h1>
-      <form id='search-container' onSubmit={(e) => search(searchText, e)}>
+      </div>
+      <textarea id="chatBox" ref={textareaRef} readOnly rows='30' value={logText}></textarea>
+      <form id='search-container' onSubmit={(e) => {
+          e.preventDefault();
+          search(searchText, e);
+          setSearchSubmitted(true);
+        }}>
         <div id='search-bar'>
           <input
             type='text'
@@ -56,7 +85,11 @@ function App() {
             {searching ? 'Searching' : 'Search'}
           </button>
         </div>
-        <div id='example-prompts'>
+        {/* {!searchSubmitted && (<div id='example-prompts'> */}
+        {(<div
+              id="example-prompts"
+              className={`fade-on-submit ${searchSubmitted ? 'fade-out' : ''}`}
+            >
           {displayedExamples.map((prompt, i) => (
             <div
               key={prompt}
@@ -69,14 +102,16 @@ function App() {
                 if (searching) return;
                 setSearchText(prompt);
                 search(prompt);
+                setSearchSubmitted(true);
               }}
             >
               "{prompt}"
             </div>
           ))}
-        </div>
-        <textarea readOnly rows='30' value={logText}></textarea>
+        </div>)}
+        
       </form>
+      </div>
     </>
   );
 }
